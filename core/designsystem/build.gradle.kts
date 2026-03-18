81f82624849622566a4bae59120d60a0565e6edd
cainan <csoliveira01@gmail.com>
2026-03-17 20:27:20 -0400

    Creating application adaptive form layout

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -10,6 +10,8 @@ kotlin {
                 implementation(libs.kotlin.stdlib)
                 // Add KMP dependencies here
 
+                implementation(projects.core.presentation)
+
                 implementation(compose.components.resources)
                 implementation(compose.components.uiToolingPreview)
             }
