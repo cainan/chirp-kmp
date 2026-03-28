da69d6f2f8361de98a9a96b26edd1a9a98181ab5
cainan <csoliveira01@gmail.com>
2026-03-27 22:14:33 -0400

    Creating login screen and navigating to register screen

--- feature/auth/presentation/src/commonMain/kotlin/com/cso/auth/presentation/di/AuthPresentationModule.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/cso/auth/presentation/di/AuthPresentationModule.kt
@@ -1,6 +1,7 @@
 package com.cso.auth.presentation.di
 
 import com.cso.auth.presentation.email_verification.EmailVerificationViewModel
+import com.cso.auth.presentation.login.LoginViewModel
 import com.cso.auth.presentation.register.RegisterViewModel
 import com.cso.auth.presentation.register_success.RegisterSuccessViewModel
 import org.koin.core.module.dsl.viewModelOf
@@ -10,4 +11,5 @@ val authPresentationModule = module {
     viewModelOf(::RegisterViewModel)
     viewModelOf(::RegisterSuccessViewModel)
     viewModelOf(::EmailVerificationViewModel)
+    viewModelOf(::LoginViewModel)
 }
