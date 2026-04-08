c5705dca58e017e63a5139215bfdef61b75d1019
cainan <csoliveira01@gmail.com>
2026-04-08 12:06:50 -0400

    Fixing problem of navigating to chat details after log in.

--- composeApp/src/commonMain/kotlin/com/cso/chirp/navigation/NavigationRoot.kt
+++ composeApp/src/commonMain/kotlin/com/cso/chirp/navigation/NavigationRoot.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import com.cso.auth.presentation.navigation.AuthGraphRoutes
 import com.cso.auth.presentation.navigation.authGraph
 import com.cso.chat.presentation.chat_list.ChatListRoute
+import com.cso.chat.presentation.navigation.ChatGraphRoutes
 import com.cso.chat.presentation.navigation.chatGraph
 
 @Composable
@@ -21,7 +22,7 @@ fun NavigationRoot(
         authGraph(
             navController = navController,
             onLoginSuccess = {
-                navController.navigate(ChatListRoute) {
+                navController.navigate(ChatGraphRoutes.Graph) {
                     popUpTo(AuthGraphRoutes.Graph) {
                         inclusive = true
                     }
