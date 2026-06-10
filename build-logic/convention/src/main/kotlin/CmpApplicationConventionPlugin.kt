3bbba33207bc4eb7dc8e416fef09890be57e531a
cainan <csoliveira01@gmail.com>
2026-06-10 19:40:59 -0400

    Creating hierarchy dependencies, and a new subset called jvmCommon

--- build-logic/convention/src/main/kotlin/CmpApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CmpApplicationConventionPlugin.kt
@@ -1,10 +1,13 @@
+import com.cso.chirp.convention.applyHierarchyTemplate
 import com.cso.chirp.convention.configureAndroidTarget
 import com.cso.chirp.convention.configureDesktopTarget
 import com.cso.chirp.convention.configureIosTargets
 import com.cso.chirp.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class CmpApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -20,6 +23,10 @@ class CmpApplicationConventionPlugin : Plugin<Project> {
             configureAndroidTarget()
             configureIosTargets()
             configureDesktopTarget()
+
+            extensions.configure<KotlinMultiplatformExtension> {
+                applyHierarchyTemplate()
+            }
 
             dependencies {
                 "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -32,30 +32,12 @@ kotlin {
             }
         }
 
-        val mobileMain by creating {
-            dependencies {
-                implementation(libs.moko.permissions)
-                implementation(libs.moko.permissions.compose)
-                implementation(libs.moko.permissions.notifications)
-            }
-            dependsOn(commonMain.get())
+        mobileMain.dependencies {
+            implementation(libs.moko.permissions)
+            implementation(libs.moko.permissions.compose)
+            implementation(libs.moko.permissions.notifications)
         }
 
-        androidMain.get().dependsOn(mobileMain)
-
-        val iosMain by creating {
-            dependsOn(mobileMain)
-        }
-
-        listOf(
-            iosArm64(),
-            iosX64(),
-            iosSimulatorArm64()
-        ).forEach { target ->
-            getByName("${target.name}Main") {
-                dependsOn(iosMain)
-            }
-        }
     }
 
 }
